27a031d4a984da87d1bddbd13a11270d7af5b178
xuanlv <xuanlv2@sapo.vn>
2025-09-17 16:29:48 +0700

    mesage

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://kiotviet.vn/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/KiotViet - Phần mềm quản lý bán hàng Phổ Biến Nhất/);
 });
 
 test('get started link', async ({ page }) => {
@@ -16,3 +16,4 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
